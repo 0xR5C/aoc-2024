5aaebec966a8ded33d1bdb91c0eb21071a81fc25
0xr5c <foths72@gmail.com>
2024-12-01 15:26:08 +0200

    Code Refactored

--- Day1/day1.go
+++ Day1/day1.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"slices"
@@ -44,6 +45,22 @@ func main() {
 	}
 	slices.Sort(list1)
 	slices.Sort(list2)
+
+	partPtr := flag.Int("part", 0, "Choose part to run")
+	flag.Parse()
+
+	if *partPtr == 1 {
+		part1(list1, list2)
+	} else if *partPtr == 2 {
+		part2(list1, list2)
+	} else {
+		part1(list1, list2)
+		part2(list1, list2)
+	}
+
+}
+
+func part1(list1, list2 []string) {
 	var dist int
 	for i := 0; i < len(list1); i++ {
 		num1, err := strconv.Atoi(list1[i])
@@ -52,5 +69,26 @@ func main() {
 		check(err)
 		dist += distInt(num1, num2)
 	}
-	fmt.Printf("Total distance is: %d\n", dist)
+	fmt.Printf("PART1 Total distance is: %d\n", dist)
+}
+
+func part2(list1, list2 []string) {
+	m := make(map[string]int)
+	for i := 0; i < len(list1); i++ {
+		m[list1[i]] = 0
+	}
+	var similarity int
+	for _, val := range list2 {
+		if _, ok := m[val]; ok {
+			m[val] += 1
+		}
+	}
+
+	for key := range m {
+		num, err := strconv.Atoi(key)
+		check(err)
+		similarity += num * m[key]
+	}
+
+	fmt.Printf("PART2 Similarity score is: %d\n", similarity)
 }
