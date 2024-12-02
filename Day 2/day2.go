7b6d004cb92e55fa7e571d41236624533fcf3f1c
0xr5c <foths72@gmail.com>
2024-12-02 23:33:07 +0200

    Day 2 - Part 2

--- Day 2/day2.go
+++ Day 2/day2.go
@@ -1,53 +1,75 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"strconv"
 	"strings"
 )
 
-
-
-
-
 func main() {
-	fmt.Println("day2")
+	fmt.Println("Day2")
 	dat, err := os.ReadFile("input.txt")
 	check(err)
 	text := string(dat[:])
 	reports := strings.Split(text, "\n")
-	// TODO - Implement flags
-	part1(reports)
-	part2(reports)
-}
 
+	partPtr := flag.Int("part", 0, "Choose part to run")
+	flag.Parse()
+
+	if *partPtr == 1 {
+		part1(reports)
+	} else if *partPtr == 2 {
+		part2(reports)
+	} else {
+		part1(reports)
+		part2(reports)
+	}
+}
 
 // PART 1
 func part1(arr []string) {
 	var safeReports int
-	for _, rep := range reports {
+	for _, rep := range arr {
 		levels := strings.Fields(rep)
 		numLevels := arrayAtoi(levels)
 		if len(numLevels) == 0 {
 			continue
 		}
-		if checkLevel(numLevels) s= true {
-			fmt.Println(numLevels)
+		ret, _ := checkLevel(numLevels)
+		if ret == true {
 			safeReports += 1
 		}
 	}
 	fmt.Printf("PART1 Safe reports are %d\n", safeReports)
 }
 
-
-
 // PART 2
 func part2(arr []string) {
-
+	var safeReports int
+	for _, rep := range arr {
+		levels := strings.Fields(rep)
+		numLevels := arrayAtoi(levels)
+		if len(numLevels) == 0 {
+			continue
+		}
+		ret, i := checkLevel(numLevels)
+		if ret == true {
+			safeReports += 1
+		} else {
+			for j := i; j >= 0; j-- {
+				singleBad := removeElement(numLevels, j)
+				ret, _ := checkLevel(singleBad)
+				if ret == true {
+					safeReports += 1
+					break
+				}
+			}
+		}
+	}
+	fmt.Printf("PART2 Safe reports are %d\n", safeReports)
 }
-
-
 
 // Utility Functions
 // TODO - Create utils package
@@ -86,7 +108,8 @@ func checkDiff(a, b int) bool {
 	}
 }
 
-func checkLevel(arr []int) bool {
+// Return true or false and index of problem if false (if true return -1)
+func checkLevel(arr []int) (bool, int) {
 	var compareFunc func(int, int) bool
 	for i := 0; i < len(arr); i++ {
 		if i == 0 {
@@ -97,13 +120,25 @@ func checkLevel(arr []int) bool {
 			} else if arr[0] < arr[1] && checkDiff(arr[0], arr[1]) {
 				compareFunc = increasing
 			} else {
-				return false
+				return false, 1
 			}
 		} else {
 			if compareFunc(arr[i-1], arr[i]) == false || !checkDiff(arr[i-1], arr[i]) {
-				return false
+				return false, i
 			}
 		}
 	}
-	return true
+	return true, -1
+}
+
+func removeElement(arr []int, index int) []int {
+	newArr := make([]int, len(arr)-1)
+	originalIndex := 0
+	for i := range arr {
+		if i != index {
+			newArr[originalIndex] = arr[i]
+			originalIndex += 1
+		}
+	}
+	return newArr
 }
